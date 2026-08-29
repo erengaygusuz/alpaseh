288b719c0b8957d7b4ce386ef76253a2d7774f5c
Eren Gaygusuz <56393357+erengaygusuz@users.noreply.github.com>
2026-08-29 15:35:22 +0300

    refactor: register game event binding service

--- Assets/Scripts/LifeTimeScopes/GameLifetimeScope.cs
+++ Assets/Scripts/LifeTimeScopes/GameLifetimeScope.cs
@@ -19,6 +19,7 @@ namespace FTRGames.Alpaseh.LifeTimeScopes
             builder.Register<GameService>(Lifetime.Scoped);
             builder.Register<GameSessionService>(Lifetime.Scoped);
             builder.Register<GameInputService>(Lifetime.Scoped);
+            builder.Register<GameEventBindingService>(Lifetime.Scoped);
             builder.Register<GameTimerService>(Lifetime.Scoped);
             builder.Register<GameFlowService>(Lifetime.Scoped);
             builder.Register<GameUIService>(Lifetime.Scoped);
