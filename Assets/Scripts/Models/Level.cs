0e01013276d97575335055f1ca978d15fd48515a
Eren Gaygusuz <56393357+erengaygusuz@users.noreply.github.com>
2026-08-29 15:06:35 +0300

    refactor: modernize level domain model

--- Assets/Scripts/Models/Level.cs
+++ Assets/Scripts/Models/Level.cs
@@ -1,87 +1,51 @@
+using System;
 using System.Collections.Generic;
 
 namespace FTRGames.Alpaseh.Models
 {
-    public class Level
+    public sealed class Level
     {
-        private float lifeIncreaseAmount;
-        private int earnedScoreAmount;
-        private float loseLifeAmount;
-        private float earnedTimeAmount;
-        private float loseTimeAmount;
-        private bool correctAnswer;
-        private int activeQuestionIndex;
-
-        public List<string> WordList { get; set; }
-
-        public Level(float lifeIncreaseAmount, int earnedScoreAmount, float loseLifeAmount, int earnedTimeAmount, int loseTimeAmount)
+        public Level(
+            float lifeIncreaseAmount,
+            int earnedScoreAmount,
+            float loseLifeAmount,
+            int earnedTimeAmount,
+            int loseTimeAmount,
+            IReadOnlyList<string> wordList)
         {
-            this.lifeIncreaseAmount = lifeIncreaseAmount;
-            this.earnedScoreAmount = earnedScoreAmount;
-            this.loseLifeAmount = loseLifeAmount;
-            this.earnedTimeAmount = earnedTimeAmount;
-            this.loseTimeAmount = loseTimeAmount;
+            WordList = wordList ?? throw new ArgumentNullException(nameof(wordList));
+            LifeIncreaseAmount = lifeIncreaseAmount;
+            EarnedScoreAmount = earnedScoreAmount;
+            LoseLifeAmount = loseLifeAmount;
+            EarnedTimeAmount = earnedTimeAmount;
+            LoseTimeAmount = loseTimeAmount;
         }
 
-        public float LifeIncreaseAmount
-        {
-            get { return lifeIncreaseAmount; }
-        }
-
-        public int EarnedScoreAmount
-        {
-            get { return earnedScoreAmount; }
-        }
-
-        public float LoseLifeAmount
-        {
-            get { return loseLifeAmount; }
-        }
-
-        public float EarnedTimeAmount
-        {
-            get { return earnedTimeAmount; }
-        }
-
-        public float LoseTimeAmount
-        {
-            get { return loseTimeAmount; }
-        }
-
-        public bool CorrectAnswer
-        {
-            get { return correctAnswer; }
-        }
-
-        public int ActiveQuestionIndex
-        {
-            get { return activeQuestionIndex; }
-            set { activeQuestionIndex = value; }
-        }
+        public IReadOnlyList<string> WordList { get; }
+        public float LifeIncreaseAmount { get; }
+        public int EarnedScoreAmount { get; }
+        public float LoseLifeAmount { get; }
+        public int EarnedTimeAmount { get; }
+        public int LoseTimeAmount { get; }
+        public bool CorrectAnswer { get; private set; }
+        public int ActiveQuestionIndex { get; private set; }
 
         public bool CheckEnteredNumberWord(string enteredNumberWord, string activeQuestionNumberWord)
         {
-            correctAnswer = true;
+            CorrectAnswer = !string.IsNullOrEmpty(enteredNumberWord) &&
+                string.Equals(enteredNumberWord, activeQuestionNumberWord, StringComparison.Ordinal);
 
-            if (enteredNumberWord.Length == 0)
-            {
-                correctAnswer = false;
-            }
+            return CorrectAnswer;
+        }
 
-            else if (enteredNumberWord.Length != activeQuestionNumberWord.Length)
-            {
-                correctAnswer = false;
-            }
+        public void AdvanceQuestion()
+        {
+            ActiveQuestionIndex++;
+        }
 
-            else
-            {
-                if (enteredNumberWord != activeQuestionNumberWord)
-                {
-                    correctAnswer = false;
-                }
-            }
-
-            return correctAnswer;
-        }  
+        public void ResetQuestionProgress()
+        {
+            ActiveQuestionIndex = 0;
+        }
     }
 }
